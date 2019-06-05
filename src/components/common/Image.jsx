b71060c1feb60c0d19c11ad9893eef8738770477
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-06-05 12:24:23 -0400

    use lightbox viewer for all images

--- src/components/common/Image.jsx
+++ src/components/common/Image.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { EditableImageUpload } from "react-easy-editables";
+import { EditableLightboxImageUpload } from "react-easy-editables";
 import Container from "./Container"
 
 import { uploadImage } from "../../firebase/operations";
@@ -9,7 +9,7 @@ export default (props) => {
   return (
     <Container>
       <div className={"my-4"}>
-        <EditableImageUpload { ...props } uploadImage={uploadImage} showCaption={true} editCaption={true} />
+        <EditableLightboxImageUpload { ...props } uploadImage={uploadImage} showCaption={true} editCaption={true} />
       </div>
     </Container>
   );

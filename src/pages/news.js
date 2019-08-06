27ea057b8b94ed83adf9148b397e0c2fc4b0919c
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-08-06 04:21:55 -0400

    news feed

--- src/pages/news.js
+++ src/pages/news.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 import { connect } from "react-redux";
 import Helmet from "react-helmet";
+import Parser from "rss-parser"
 
 import { DEFAULT_COMPONENT_CONTENT } from '../utils/constants';
 import {
@@ -57,7 +58,7 @@ const mapStateToProps = state => {
   };
 };
 
-class ResourcesPage extends React.Component {
+class NewsPage extends React.Component {
 
   constructor(props) {
     super(props);
@@ -66,7 +67,24 @@ class ResourcesPage extends React.Component {
       content: JSON.parse(this.props.data.pages.content)
     };
     this.props.onLoadPageData(initialPageData);
+    this.state = {
+      items: []
+    }
   };
+
+  componentDidMount() {
+    const parser = new Parser()
+    const cors_proxy = "https://cors-anywhere.herokuapp.com/"
+    const url = 'https://news.google.com/rss/search?q=Sarnia+OR+aamjiwnaang+OR+%22imperial+oil%22+OR+%22chemical+valley%22)+AND+(spill+OR+leak+OR+flare+OR+pollution+Or+release+-%22news+release%22)&hl=en-CA&gl=CA&ceid=CA:en'
+    const feed = parser.parseURL(cors_proxy + url, (err, data) => {
+      if (err) {
+        return console.error(err)
+      }
+
+      this.setState({ items: data.items })
+    })
+
+  }
 
   onSave = id => content => {
     this.props.onUpdatePageData(PAGE_ID, id, content);
@@ -91,6 +109,17 @@ class ResourcesPage extends React.Component {
   render() {
     const pageData = this.props.pageData ? this.props.pageData : this.props.data.pages;
     const content = this.props.pageData ? this.props.pageData.content : JSON.parse(this.props.data.pages.content);
+    let orderedNewsItems = [...this.state.items]
+    orderedNewsItems.sort((a,b) => {
+      const time1 = new Date(a.pubDate).getTime();
+      const time2 = new Date(b.pubDate).getTime();
+      if (time1 > time2) {
+        return -1
+      } else {
+        return 1
+      }
+      return 0
+    })
 
     return (
       <Layout location={this.props.location}>
@@ -131,16 +160,19 @@ class ResourcesPage extends React.Component {
               <h2 data-animation="fadeInUp" data-delay=".5s" className="mb-4">
                 <EditableText content={content["references-title"]} onSave={this.onSave("references-title")} />
               </h2>
-              <div className="md-mt-5">
-                <Collection
-                  items={content["references"]}
-                  Component={Reference}
-                  onSave={this.onSave('references')}
-                  onAddItem={this.onAddItem('references')}
-                  onDeleteItem={this.onDeleteItem('references')}
-                  isEditingPage={this.props.isEditingPage}
-                  defaultContent={DEFAULT_COMPONENT_CONTENT['references']}
-                />
+              <div className="mt-30">
+                {
+                  orderedNewsItems.map(item => {
+                    const date = new Date(item.pubDate)
+                    const dateString = date.toLocaleDateString('en-CA', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+                    return(
+                      <div className="news-item mb-30 pb-10" key={item.guid}>
+                        <p className="text-muted date">{dateString}</p>
+                        <div dangerouslySetInnerHTML={ {__html: item.content} } />
+                      </div>
+                    )
+                  })
+                }
               </div>
             </Container>
           </Section>
@@ -150,7 +182,7 @@ class ResourcesPage extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ResourcesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(NewsPage);
 
 export const query = graphql`
   query {

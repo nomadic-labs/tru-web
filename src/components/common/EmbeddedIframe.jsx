d846d6603385e913dc37de3b6762e5ada752e1b2
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-08-21 23:17:30 -0400

    fix google map scrolling problem

--- src/components/common/EmbeddedIframe.jsx
+++ src/components/common/EmbeddedIframe.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import { EditableEmbeddedIframe } from "react-easy-editables";
 import Container from "./Container"
 
+class EmbeddedIframe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { scrollEnabled: false }
+  }
 
-const EmbeddedIframe = props => {
-  return (
-    <div className={`iframe-container my-4 ${props.classes || ''}`}>
-      <EditableEmbeddedIframe { ...props } />
-    </div>
-  );
-};
+  enableScroll = () => { this.setState({ scrollEnabled: true }) }
+
+  disableScroll = () => { this.setState({ scrollEnabled: false }) }
+
+  render() {
+    return (
+      <div
+        onClick={this.enableScroll}
+        onMouseLeave={this.disableScroll}
+        className={`iframe-container my-4 ${this.props.classes || ''} ${this.state.scrollEnabled ? "clicked" : ""}`}
+      >
+        <EditableEmbeddedIframe { ...this.props } />
+      </div>
+    );
+  }
+}
 
 export default EmbeddedIframe;

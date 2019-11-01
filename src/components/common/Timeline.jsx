2305632c350d9244bb2c9260eaec064ca2874442
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-11-01 11:39:26 -0400

    update timeline style

--- src/components/common/Timeline.jsx
+++ src/components/common/Timeline.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "./Container"
-import { EditableKnightTimeline } from "react-easy-editables"
+import { EditableTimeline } from "react-easy-editables"
 
 import "../../assets/sass/timeline.scss"
 
@@ -10,7 +10,7 @@ const Timeline = props => {
   return (
     <Container xs={props.xs || 12} sm={props.sm || 12} md={props.md || 12} lg={props.lg || 12} xl={props.xl || 12}>
       <div>
-        <EditableKnightTimeline { ...props } options={{ initial_zoom: 6 }} />
+        <EditableTimeline { ...props } />
       </div>
     </Container>
   );
